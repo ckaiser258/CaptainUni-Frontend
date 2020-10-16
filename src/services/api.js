0cac7784e1b3a710d427ab428564cbfe3db9b3d2
Colton <kaiserc2@seattleu.edu>
2020-10-16 14:26:35 -0700

    Add get and create functions for users in api.js.

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,15 @@ const API_ROOT = `localhost:3000/api/v1`;
 
 const token = () => localStorage.getItem("token");
 
+const userObj = (data) => {
+    return {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        username: data.username,
+        password: data.password
+    }
+}
+
 const headers = () => {
   return {
     "Content-Type": "application/json",
@@ -10,11 +19,30 @@ const headers = () => {
   };
 };
 
-//Validate login credentials, exchange JWT token, render JSON
+//Validate login credentials, exchange JWT token, send POST request for new session
 const login = (data) => {
   return fetch(`${API_ROOT}/login`, {
     method: "POST",
     headers: headers(),
-    body: JSON.stringify({ user: data }),
+    body: JSON.stringify({ "user": data }),
   }).then((res) => res.json());
 };
+
+//Fetch user after login, render JSON
+const getCurrentUser = () => {
+    return fetch(`${API_ROOT}/profile`, {
+        headers: headers()
+    })
+    .then(res => res.json())
+}
+
+//Grab user params and send POST request to create new user
+const createUser = data => {
+    let obj = userObj(data)
+    return fetch(`${API_ROOT}/users`, {
+        method: "POST",
+        headers: headers(),
+        body: JSON.stringify({ "user": obj })
+    })
+    .catch(error => alert(error.message))
+}
