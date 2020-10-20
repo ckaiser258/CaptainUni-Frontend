29f33833c8219b729156da8699fc2c0308ecd81a
Colton <kaiserc2@seattleu.edu>
2020-10-19 20:04:00 -0700

    Add login() function to login page.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,40 +1,60 @@
 import React, { useState } from "react";
-import {api} from '../services/api'
+import { api } from "../services/api";
 import { Container, Form, Button } from "react-bootstrap";
 
 const { Group, Label, Control } = Form;
 
 function LoginPage() {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
 
-    const [user, setUser] = useState({})
-    const [error, setError] = useState(false)
+  //Set user state to username and passwords fields
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-    //Set user state to username and passwords fields
-    const handleChange = (e) => {
-        setUser({...user, [e.target.name]: e.target.value})
-    }
-    
+  //Verify user and then issue token, or throw error
+  const login = (e) => {
+    e.preventDefault();
+    setUser({});
+    api.auth.login(user).then((res) => {
+      if (res.message) return setError(res.message);
+      localStorage.setItem("token", res.jwt);
+    });
+  };
+
   return (
     <>
-      <div style={{marginTop: 50}}>
+      <div style={{ marginTop: 50 }}>
         <h1>Welcome To CaptainUni</h1>
       </div>
       <div>
         <Container style={{ marginTop: 200, width: "30%" }}>
-          <Form>
+          <Form onSubmit={login}>
             <Group>
               <Label>Username</Label>
-              <Control name="username" type="email" placeholder="Enter username" onChange={handleChange}/>
+              <Control
+                name="username"
+                type="name"
+                placeholder="Enter username"
+                onChange={handleChange}
+              />
             </Group>
             <Group>
               <Label>Password</Label>
-              <Control name="password" type="password" placeholder="Enter password" onChange={handleChange}/>
+              <Control
+                name="password"
+                type="password"
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
             </Group>
             <Button variant="primary" type="submit">
               Log In
             </Button>
           </Form>
         </Container>
+        {error ? <h5>{error}</h5> : null}
       </div>
     </>
   );
