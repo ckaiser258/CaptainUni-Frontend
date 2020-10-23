6d411bbf7cbe6243654d400a929a56cc05c2bfd5
Colton <kaiserc2@seattleu.edu>
2020-10-23 15:39:08 -0700

    Change navbar brand styling.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Button } from "react-bootstrap";
 
-function NavBar({ firstName }) {
+function NavBar({ user, logout }) {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
-      <Navbar.Brand style={{ marginLeft: "auto" }}>
-        {firstName ? `Hello, ${firstName}` : null}
+      <Navbar.Brand href={user ? "/athletes" : null}>
+        <strong>CAPTAINUni</strong>
       </Navbar.Brand>
+      <Navbar.Brand style={{ marginLeft: "auto" }}>
+        {user.first_name ? `Hello, ${user.first_name}` : null}
+      </Navbar.Brand>
+      {user.id ? (
+        <Button href="/" variant="outline-info" size="sm" onClick={logout}>
+          Log Out
+        </Button>
+      ) : null}
     </Navbar>
   );
 }
