d3cc6020d49e8087ff473da318a4e88495a37415
Colton <kaiserc2@seattleu.edu>
2020-10-24 14:54:03 -0700

    Modify layout of athlete profile page.

--- src/components/AthleteProfile.js
+++ src/components/AthleteProfile.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { api } from "../services/api";
 import AddOrEditAthleteForm from "./AddOrEditAthleteForm";
+import { Row, Col, Container } from "react-bootstrap";
+import {Typography} from "@material-ui/core"
 
 function AthleteProfile({ id, fetchAthletes }) {
   const [athlete, setAthlete] = useState({});
 
   const fetchAthlete = () => {
-    api.athletes.getAthlete(id).then(res => {
-      setAthlete(res.athlete)
-    })
-  }
+    api.athletes.getAthlete(id).then((res) => {
+      setAthlete(res.athlete);
+    });
+  };
 
   //Use props to set the current athlete upon component mount
   //to change state upon edit
   useEffect(() => {
-    fetchAthlete()
+    fetchAthlete();
     return () => {
       setAthlete({});
     };
@@ -23,35 +25,78 @@ function AthleteProfile({ id, fetchAthletes }) {
   //Passed down to edit form and will pass params
   //back up here to state level
   const editAthlete = (data) => {
-    api.athletes.editAthlete(data).then(setAthlete({ ...athlete, ...data })
-    )
+    api.athletes.editAthlete(data).then(setAthlete({ ...athlete, ...data }));
+  };
+
+  const handleImageError = (e) => {
+    e.target.src = "https://www.w3schools.com/howto/img_avatar.png";
   };
 
   return (
     <>
-      {/* Edit form is either showing or pencil button */}
-      <AddOrEditAthleteForm
-        athleteId={athlete.id}
-        athleteName={athlete.full_name}
-        editAthlete={editAthlete}
-      />
-      <div>
-        <h1>{athlete.full_name}</h1>
-        <img src={athlete.image} alt={athlete.full_name + "'s photo"} />
-      </div>
-      <div>
-        <strong>Phone Number</strong> {athlete.phone_number}
-        <strong>Address</strong> {athlete.address}
-        <strong>Height</strong> {athlete.height}
-        <strong>Weight</strong> {athlete.weight}
-        <strong>Birthdate</strong> {athlete.birthday}
-        <strong>Position:</strong> {athlete.positions}
-      </div>
-      <div>
-        <strong>High School</strong> {athlete.high_school}
-        <strong>Graduation Year</strong> {athlete.graduation_year}
-        <strong>GPA</strong> {athlete.gpa}
-      </div>
+      <Container>
+        <Row style={{paddingTop: 20}}>
+          <Col md="auto" style={{paddingLeft: 20}}>
+            <Typography variant="h3">{athlete.full_name}</Typography>
+          </Col>
+          <Col md="auto" style={{paddingLeft: 0}}>
+            {/* If form is false, renders a pencil button */}
+            <AddOrEditAthleteForm
+              athleteId={athlete.id}
+              athleteName={athlete.full_name}
+              editAthlete={editAthlete}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <hr />
+      <Container>
+        <Row>
+          <Col>
+            <img
+              style={{ maxHeight: 250, maxWidth: 250 }}
+              src={athlete.image || "https://www.w3schools.com/howto/img_avatar.png"}
+              alt={athlete.full_name + "'s photo"}
+              onError={handleImageError}
+            />
+          </Col>
+          <Col md="auto">
+            <p>
+              <strong>Phone Number</strong> {athlete.phone_number}
+            </p>
+            <p>
+              <strong>Address</strong> {athlete.address}
+            </p>
+            <p>
+              <strong>Height</strong> {athlete.height}"
+            </p>
+            <p>
+              <strong>Weight</strong> {athlete.weight}
+            </p>
+            <p>
+              <strong>Birthdate</strong> {athlete.birthday}
+            </p>
+            <p>
+              <strong>Position:</strong> {athlete.positions}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+        <Typography variant="h4" style={{marginLeft: 20, float: "left"}}>Academics</Typography>
+      </Row>
+      <hr />
+            <p>
+              <strong>High School</strong> {athlete.high_school}
+            </p>
+            <p>
+              <strong>Graduation Year</strong> {athlete.graduation_year}
+            </p>
+            <p>
+              <strong>GPA</strong> {athlete.gpa}
+            </p>
+      </Container>
     </>
   );
 }
